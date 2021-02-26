d40ed04cd86d574260392d1ed05d750774cb2d78
Kogan <54910934+02ks@users.noreply.github.com>
2021-02-26 15:45:49 -0800

    Update RGBstripwmic.ino

--- RGBstripwmic/RGBstripwmic.ino
+++ RGBstripwmic/RGBstripwmic.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 #define LED_PIN    6
@@ -22,11 +22,16 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // setup() function -- runs once at startup --------------------------------
 const int sampleWindow = 5000; // Sample window width in mS (250 mS = 4Hz)
-unsigned int knock;
+unsigned int knock1;
+unsigned int knock2;
+unsigned int knock3;
+unsigned int knock4;
+unsigned int knock5;
+unsigned int knocktotal;
 int clapcount = 0;
-
+int fun = 0;
 void setup() {
-  
+
   Serial.begin(9600);
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
@@ -50,29 +55,35 @@ void loop() {
   unsigned int signalMin = maxvol;
 
   // collect data for 250 miliseconds
- 
-  knock = analogRead(0);
-  double volts = (knock * 5) / maxvol;  // convert to volts
+
+  knock1 = analogRead(0);
+  knock2 = analogRead(0);
+  knock3 = analogRead(0);
+  knock4 = analogRead(0);
+  knock5 = analogRead(0);
+
+  knocktotal = knock1 + knock2 + knock3 + knock4 + knock5;
+  double volts = (knocktotal) / maxvol;  // convert to volts
   Serial.println(volts);
-  if (volts < 1) { //might need to change this variable
+  delay(100);
+  if (volts < 2.4) { //might need to change this variable
+
     clapcount = clapcount + 1;
     Serial.println(clapcount);
     delay(500);
     if (clapcount > 0) {
       if (clapcount == 1) {
-        colorWipe(strip.Color(  0,   0, 255), 50); // Blue
-        delay(1000);
+        colorOn(50); // Blue
       }
       /**else if (clapcount == 2) {
-        delay(1000);
+
       }
-      else if (clapcount == 3) {
+      /**else if (clapcount == 3) {
         delay(1000);
-      }**/
+        }**/
       else {
         clapcount = 0;
         colorWipe(strip.Color(  0,   0,  0), 50); // Blue
-        delay(1000);
       }
     }
   }
@@ -86,73 +97,101 @@ void loop() {
 // (as a single 'packed' 32-bit value, which you can get by calling
 // strip.Color(red, green, blue) as shown in the loop() function above),
 // and a delay time (in milliseconds) between pixels.
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+void colorOn(int wait) {
+  while (fun < 50) {
+    fun = fun + 1;
+    int i = rand() % 59 + 1;
+    int i2 = rand() % 59 + 1;
+    int i3 = rand() % 59 + 1;
+    int i4 = rand() % 59 + 1;
+    int r = rand() % 255;
+    int g = rand() % 255;
+    int b = rand() % 255;
+    uint32_t color = strip.Color(  r,   g,  b);
     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
+    strip.show();   //  Update strip to match
+    strip.setPixelColor(i2, color);         //  Set pixel's color (in RAM)
+    strip.show();   //  Update strip to match
+    strip.setPixelColor(i3, color);         //  Set pixel's color (in RAM)
+    strip.show();   //  Update strip to match
+    strip.setPixelColor(i4, color);         //  Set pixel's color (in RAM)
+    strip.show();   //  Update strip to match
+    delay(50);
+    strip.clear();         //  Set pixel's color (in RAM)
+    strip.show();   //  Update strip to match
+    delay(wait);
   }
-}
+  if (fun >= 50) { 
+    fun = 0;
+  }
+  }
+  void colorWipe(uint32_t color, int wait) {
+    for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
+      strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+      strip.show();                          //  Update strip to match
+      delay(wait);                           //  Pause for a moment
+    }
+  }
 
-// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
-// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
-// between frames.
-void theaterChase(uint32_t color, int wait) {
-  for(int a=0; a<10; a++) {  // Repeat 10 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in steps of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+  // Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+  // a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+  // between frames.
+  void theaterChase(uint32_t color, int wait) {
+    for (int a = 0; a < 10; a++) { // Repeat 10 times...
+      for (int b = 0; b < 3; b++) { //  'b' counts from 0 to 2...
+        strip.clear();         //   Set all pixels in RAM to 0 (off)
+        // 'c' counts up from 'b' to end of strip in steps of 3...
+        for (int c = b; c < strip.numPixels(); c += 3) {
+          strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+        }
+        strip.show(); // Update strip with new contents
+        delay(wait);  // Pause for a moment
+      }
+    }
+  }
+
+  // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+  void rainbow(int wait) {
+    // Hue of first pixel runs 5 complete loops through the color wheel.
+    // Color wheel has a range of 65536 but it's OK if we roll over, so
+    // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+    // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+    for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
+      for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
+        // Offset pixel hue by an amount to make one full revolution of the
+        // color wheel (range of 65536) along the length of the strip
+        // (strip.numPixels() steps):
+        int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+        // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+        // optionally add saturation and value (brightness) (each 0 to 255).
+        // Here we're using just the single-argument hue variant. The result
+        // is passed through strip.gamma32() to provide 'truer' colors
+        // before assigning to each pixel:
+        strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
       }
       strip.show(); // Update strip with new contents
       delay(wait);  // Pause for a moment
     }
   }
-}
 
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
-  }
-}
-
-// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-void theaterChaseRainbow(int wait) {
-  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-  for(int a=0; a<30; a++) {  // Repeat 30 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in increments of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        // hue of pixel 'c' is offset by an amount to make one full
-        // revolution of the color wheel (range 65536) along the length
-        // of the strip (strip.numPixels() steps):
-        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+  // Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+  void theaterChaseRainbow(int wait) {
+    int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+    for (int a = 0; a < 30; a++) { // Repeat 30 times...
+      for (int b = 0; b < 3; b++) { //  'b' counts from 0 to 2...
+        strip.clear();         //   Set all pixels in RAM to 0 (off)
+        // 'c' counts up from 'b' to end of strip in increments of 3...
+        for (int c = b; c < strip.numPixels(); c += 3) {
+          // hue of pixel 'c' is offset by an amount to make one full
+          // revolution of the color wheel (range 65536) along the length
+          // of the strip (strip.numPixels() steps):
+          int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+          uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+          strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+        }
+        strip.show();                // Update strip with new contents
+        delay(wait);                 // Pause for a moment
+        firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
       }
-      strip.show();                // Update strip with new contents
-      delay(wait);                 // Pause for a moment
-      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
     }
   }
-}
